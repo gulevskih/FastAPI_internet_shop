ac3b9bbf5c2ae236dc43ab373ba850d39a398d4a
Stanislav <gulevskih@mail.ru>
2026-01-26 22:35:10 +0300

    переход на PostgreSQL

--- app/database.py
+++ app/database.py
@@ -14,3 +14,20 @@ SessionLocal = sessionmaker(bind=engine)
 # Определяем базовый класс для моделей
 class Base(DeclarativeBase):
     pass
+
+# --------------- Асинхронное подключение к PostgreSQL -------------------------
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:bloodSoul13!@localhost:5432/ecommerce_db"
+
+# Создаём Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
+class Base(DeclarativeBase):
+    pass
